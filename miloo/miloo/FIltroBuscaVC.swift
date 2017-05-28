9271b6ba2b5e91784b86c3c7aa70c901dedaf95d
Andre Rosa <andre.i.r@live.com>
2017-05-28 03:10:16 -0300

    tela de filtro de buscas e novo logo

---
+++ miloo/miloo/FIltroBuscaVC.swift
@@ -0,0 +1,39 @@
+//
+//  FIltroBuscaVC.swift
+//  miloo
+//
+//  Created by Andre Rosa on 28/05/17.
+//  Copyright © 2017 Andre Rosa. All rights reserved.
+//
+
+import UIKit
+
+class FIltroBuscaVC: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+    @IBAction func fecharFiltro(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func aplicarFiltro(_ sender: Any) {
+    }
+  
+    @IBAction func filtroOrdem(_ sender: Any) {
+    }
+    
+    @IBAction func filtroAtendimento(_ sender: Any) {
+    }
+
+    @IBAction func filtroPagamento(_ sender: Any) {
+    }
+  
+    @IBAction func limparFiltros(_ sender: Any) {
+    }
+
+}
