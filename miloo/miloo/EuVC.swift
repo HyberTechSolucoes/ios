9784cbd64d9124d2aa9cdae24dcb98eb60c5298a
Andre Rosa <andre.i.r@live.com>
2017-06-26 09:52:55 -0300

    ajustes

---
+++ miloo/miloo/EuVC.swift
@@ -0,0 +1,21 @@
+//
+//  EuVC.swift
+//  miloo
+//
+//  Created by Andre Rosa on 25/06/17.
+//  Copyright © 2017 Andre Rosa. All rights reserved.
+//
+
+import UIKit
+
+class EuVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+   
+    
+
+}
--- miloo/miloo/infoProfissionalBuscaTVC.swift
+++ miloo/miloo/infoProfissionalBuscaTVC.swift
@@ -1,95 +1,46 @@
 //
-//  infoProfissionalBuscaTVC.swift
+//  InfoProfissionalTVC.swift
 //  miloo
 //
-//  Created by Andre Rosa on 25/06/17.
+//  Created by Andre Rosa on 16/06/17.
 //  Copyright © 2017 Andre Rosa. All rights reserved.
 //
 
 import UIKit
 
-class infoProfissionalBuscaTVC: UITableViewController {
-
+class InfoProfissionalBuscaTVC: UITableViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var imgsGaleria = [GaleriaProfissinal]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        for _ in 0...10{
+            imgsGaleria.append(GaleriaProfissinal(imagem: "informatica_cover"))
+        }
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imgsGaleria.count
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImgGaleriaProfissionalCVC", for: indexPath) as? ImgGaleriaProfissionalCVC{
+            let imgGaleria = imgsGaleria[indexPath.row]
+            
+            cell.updateGaleria(GaleriaProfissinal: imgGaleria)
+            
+            return cell
+            
+        } else{
+            return UICollectionViewCell()
+        }
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
